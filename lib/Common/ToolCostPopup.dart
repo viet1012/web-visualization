49e24ce2644e5f827c773e7043efd1ce41e0b71f
Viet Ta <89030236+viet1012@users.noreply.github.com>
2025-04-23 10:38:47 +0700

    wip

--- lib/Common/ToolCostPopup.dart
+++ lib/Common/ToolCostPopup.dart
@@ -25,10 +25,10 @@ class _ToolCostPopupState extends State<ToolCostPopup> {
   void initState() {
     super.initState();
     filteredData = widget.data;
-    _filterController.addListener(_applyFilter);
+    _filterController.addListener(_applyFilterForInputField);
   }
 
-  void _applyFilter() {
+  void _applyFilterForInputField() {
     final query = _filterController.text.toLowerCase();
     setState(() {
       filteredData =
@@ -47,6 +47,10 @@ class _ToolCostPopupState extends State<ToolCostPopup> {
     _filterController.dispose();
     _scrollController.dispose();
     super.dispose();
+  }
+
+  List<String> _getUniqueValues(String Function(DetailsDataModel) selector) {
+    return widget.data.map(selector).toSet().toList()..sort();
   }
 
   @override
@@ -80,6 +84,18 @@ class _ToolCostPopupState extends State<ToolCostPopup> {
     );
   }
 
+
+  void _resetFilter() {
+    setState(() {
+      _filterController.clear();
+      selectedXblnr2 = null;
+      selectedMaktx = null;
+      selectedMatnr = null;
+      selectedDept = null;
+      filteredData = widget.data;
+    });
+  }
+
   Widget _buildHeader(ThemeData theme) {
     // Tính tổng amount
     final totalAmount = filteredData.fold<double>(
@@ -90,6 +106,7 @@ class _ToolCostPopupState extends State<ToolCostPopup> {
     return Column(
       children: [
         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Expanded(
               child: Align(
@@ -107,11 +124,26 @@ class _ToolCostPopupState extends State<ToolCostPopup> {
             Row(
               children: [
                 Text(
-                  "Total: ${(totalAmount / 1000).toStringAsFixed(2)}K\$",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
+                  "Total: ${(totalAmount / 1000).toStringAsFixed(1)}K\$",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                SizedBox(width: 16),
+                FilledButton.icon(
+                  icon: Icon(Icons.refresh_rounded),
+                  label: Text('Refresh'),
+                  style: FilledButton.styleFrom(
+                    backgroundColor: Colors.orange.shade700,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                    textStyle: theme.textTheme.labelLarge?.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                    elevation: 2,
                   ),
+                  onPressed: _resetFilter,
                 ),
                 SizedBox(width: 16),
                 FilledButton.icon(
@@ -161,13 +193,92 @@ class _ToolCostPopupState extends State<ToolCostPopup> {
     );
   }
 
+  String? selectedDept;
+  String? selectedMatnr;
+  String? selectedMaktx;
+  String? selectedXblnr2;
+
+  void _applyFilter() {
+    final query = _filterController.text.toLowerCase();
+    setState(() {
+      filteredData =
+          widget.data.where((item) {
+            final matchesSearch =
+                item.dept.toLowerCase().contains(query) ||
+                item.maktx.toLowerCase().contains(query) ||
+                item.xblnr2.toLowerCase().contains(query) ||
+                item.bktxt.toLowerCase().contains(query) ||
+                item.matnr.toLowerCase().contains(query);
+
+            final matchesDept =
+                selectedDept == null || item.dept == selectedDept;
+            final matchesMatnr =
+                selectedMatnr == null || item.matnr == selectedMatnr;
+            final matchesMaktx =
+                selectedMaktx == null || item.maktx == selectedMaktx;
+            final matchesXblnr2 =
+                selectedXblnr2 == null || item.xblnr2 == selectedXblnr2;
+
+            return matchesSearch && matchesDept && matchesMatnr && matchesMaktx && matchesXblnr2;
+          }).toList();
+    });
+  }
+
+  Widget _buildDropdownHeader({
+    required String title,
+    required String? selectedValue,
+    required List<String> values,
+    required Function(String?) onChanged,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Column(
+        children: [
+          DropdownButton<String>(
+            value: selectedValue ?? '__reset__',
+            isExpanded: true,
+            items: [
+              DropdownMenuItem<String>(
+                value: '__reset__',
+                child: Padding(
+                  padding:  EdgeInsets.only(left: 8) ,
+                  child: Center(
+                    child: Text(
+                      title,
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                  ),
+                ),
+              ),
+              ...values.map(
+                (v) => DropdownMenuItem<String>(
+                  value: v,
+                  child: Center(
+                    child: Text(
+                      v,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+            onChanged: onChanged,
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget _buildDataTable(BuildContext context, ThemeData theme) {
-    return Column(
-      children: [
-        // Sticky Header (dòng tiêu đề)
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Table(
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Column(
+        children: [
+          // Sticky Header (dòng tiêu đề)
+          Table(
             border: TableBorder.all(color: theme.dividerColor.withOpacity(0.8)),
             columnWidths: {
               0: FixedColumnWidth(120),
@@ -177,17 +288,38 @@ class _ToolCostPopupState extends State<ToolCostPopup> {
               4: FixedColumnWidth(220),
               5: FixedColumnWidth(220),
               6: FixedColumnWidth(100),
-              7: FixedColumnWidth(80),
+              7: FixedColumnWidth(100),
               8: FixedColumnWidth(120),
             },
             children: [
               TableRow(
                 children: [
                   _buildTableCell('Dept', isHeader: true),
-                  _buildTableCell('Material No.', isHeader: true),
+                  _buildDropdownHeader(
+                    title: 'Material No',
+                    selectedValue: selectedMatnr,
+                    values: _getUniqueValues((e) => e.matnr),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedMatnr = value;
+                        _applyFilter();
+                      });
+                    },
+                  ),
                   _buildTableCell('Description', isHeader: true),
                   _buildTableCell('Used Date', isHeader: true),
-                  _buildTableCell('Doc Number', isHeader: true),
+                  // _buildTableCell('Doc Number', isHeader: true),
+                  _buildDropdownHeader(
+                    title: 'Doc Number',
+                    selectedValue: selectedXblnr2,
+                    values: _getUniqueValues((e) => e.xblnr2),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedXblnr2 = value;
+                        _applyFilter();
+                      });
+                    },
+                  ),
                   _buildTableCell('Note', isHeader: true),
                   _buildTableCell('Qty', isHeader: true),
                   _buildTableCell('Unit', isHeader: true),
@@ -196,74 +328,79 @@ class _ToolCostPopupState extends State<ToolCostPopup> {
               ),
             ],
           ),
-        ),
-        // Scrollable content
-        Expanded(
-          child: Scrollbar(
-            controller: _scrollController,
-            thumbVisibility: true,
-            thickness: 10,
-            radius: const Radius.circular(5),
-            child: SingleChildScrollView(
+          // Scrollable content
+          Expanded(
+            child: Scrollbar(
               controller: _scrollController,
-              scrollDirection: Axis.vertical,
+              thumbVisibility: true,
+              thickness: 10,
+              radius: const Radius.circular(5),
               child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Table(
-                  border: TableBorder.all(
-                    color: theme.dividerColor.withOpacity(0.8),
+                controller: _scrollController,
+                scrollDirection: Axis.vertical,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Table(
+                    border: TableBorder.all(
+                      color: theme.dividerColor.withOpacity(0.8),
+                    ),
+                    columnWidths: {
+                      0: FixedColumnWidth(120),
+                      1: FixedColumnWidth(150),
+                      2: FixedColumnWidth(500),
+                      3: FixedColumnWidth(150),
+                      4: FixedColumnWidth(220),
+                      5: FixedColumnWidth(220),
+                      6: FixedColumnWidth(100),
+                      7: FixedColumnWidth(100),
+                      8: FixedColumnWidth(120),
+                    },
+                    children:
+                        filteredData.map((item) {
+                          return TableRow(
+                            children: [
+                              _buildTableCell(item.dept),
+                              _buildTableCell(item.matnr),
+                              _buildTableCell(item.maktx),
+                              _buildTableCell(item.useDate),
+                              _buildTableCell(item.xblnr2),
+                              _buildTableCell(item.bktxt),
+                              _buildTableCell(item.qty.toString()),
+                              _buildTableCell(item.unit),
+                              _buildTableCell(item.amount.toStringAsFixed(2)),
+                            ],
+                          );
+                        }).toList(),
                   ),
-                  columnWidths: {
-                    0: FixedColumnWidth(120),
-                    1: FixedColumnWidth(150),
-                    2: FixedColumnWidth(500),
-                    3: FixedColumnWidth(150),
-                    4: FixedColumnWidth(220),
-                    5: FixedColumnWidth(220),
-                    6: FixedColumnWidth(100),
-                    7: FixedColumnWidth(80),
-                    8: FixedColumnWidth(120),
-                  },
-                  children:
-                      filteredData.map((item) {
-                        return TableRow(
-                          children: [
-                            _buildTableCell(item.dept),
-                            _buildTableCell(item.matnr),
-                            _buildTableCell(item.maktx),
-                            _buildTableCell(item.useDate),
-                            _buildTableCell(item.xblnr2),
-                            _buildTableCell(item.bktxt),
-                            _buildTableCell(item.qty.toString()),
-                            _buildTableCell(item.unit),
-                            _buildTableCell(item.amount.toStringAsFixed(2)),
-                          ],
-                        );
-                      }).toList(),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
   Widget _buildTableCell(
     String text, {
     bool isHeader = false,
-    bool numeric = false,
     bool highlight = false,
   }) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Text(
-        text,
-        textAlign: isHeader ? TextAlign.center : TextAlign.center,
-        style: TextStyle(
-          fontWeight: isHeader ? FontWeight.w600 : FontWeight.normal,
-          color: highlight ? Colors.blue.shade700 : null,
-          fontSize: isHeader ? 18 : 16,
+    return Center(
+      child: Container(
+        padding: isHeader ? EdgeInsets.only(top: 8) : null,
+        // color: Colors.green,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            text,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: isHeader ? FontWeight.w600 : FontWeight.normal,
+              color: highlight ? Colors.blue.shade700 : null,
+              fontSize: isHeader ? 18 : 16,
+            ),
+          ),
         ),
       ),
     );
